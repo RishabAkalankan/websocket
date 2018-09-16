6bfc9732a021411ee5eb1f003190010cf4eba35a
rishab <rishabkumara@gmail.com>
2018-09-16 17:50:16 +0530

    Emit messages

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    socket.emit('newMessage', {
-        from: 'daenerys@atod.com',
-        message: 'Im coming you pussy',
-        createdAt: new Date().getTime()
-    });
-
     socket.on('createMessage', (message) => {
         console.log(message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
     socket.on('disconnect', () => {
         console.log('user was disconnected');
